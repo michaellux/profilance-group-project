19a988ef30fa5c2686f6a1df00d69af8d34037d8
michaellux <michaellux@yandex.ua>
2020-11-25 13:43:28 +0300

    add primary content for pages

---
+++ src/components/pages/home-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './pages.css';
+
+const HomePage = () => {
+  return <h1 className="title main__home-title">Home Page</h1>;
+}
+
+export default HomePage;
---
+++ src/components/pages/news-page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './pages.css';
+
+const NewsPage = () => {
+  return <h1 className="title main__news-title">News Page</h1>
+};
+
+export default NewsPage;
