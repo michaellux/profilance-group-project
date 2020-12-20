d869e573389b604a6918c293b5bb907c030d1397
michaellux <michaellux@yandex.ua>
2020-12-20 14:36:44 +0300

    create client for News API | https://newsapi.org/

--- src/services/news-service.js
+++ src/services/news-service.js
@@ -1,5 +1,22 @@
 export default class NewsService {
-  getNews() {
-    return [];
+  constructor() {
+    this.req = null;
+    this.apiBase = 'http://newsapi.org/v2';
+  }
+
+  async getResource(url) {
+    this.req = new Request(`${this.apiBase}${url}`);
+    return fetch(this.req)
+      .then((res) => res.json()
+        .then((json) => json));
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async getNews() {
+    const result = await this
+      .getResource('/top-headlines?country=ru&apiKey=c964acebddc6420d941a6df3bc26a73f')
+      .then((json) => json.articles);
+
+    return result;
   }
 }
