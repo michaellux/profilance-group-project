a614efe94cb7fa0e8b9057a7e4de91f1cff8a976
michaellux <michaellux@yandex.ua>
2020-12-20 23:37:58 +0300

    add title

--- src/components/pages/news-page.jsx
+++ src/components/pages/news-page.jsx
@@ -4,6 +4,11 @@ import './pages.css';
 import NewsList from '../news-list';
 
 // eslint-disable-next-line no-unused-vars
-const NewsPage = () => <NewsList />;
+const NewsPage = () => (
+  <>
+    <h1 className="title main__news-title">Новости</h1>
+    <NewsList />
+  </>
+);
 
 export default NewsPage;
