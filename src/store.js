c7b3875a202a1199fbf0e3a6e5df9f3f2ab14636
michaellux <michaellux@yandex.ua>
2020-12-20 14:38:31 +0300

    add support for the redux-devtools extension

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-underscore-dangle */
 import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
